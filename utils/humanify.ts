fdc7f7a167a5c9e9b70239052cb6f470cf52408d
Claire Froelich <claire.froelich@gmail.com>
2025-04-18 16:44:22 -0400

    rm comment

--- utils/humanify.ts
+++ utils/humanify.ts
@@ -1,5 +1,5 @@
 function humanify(bytes: number): string {
-	const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]; // Units in binary order
+	const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 	let size = bytes;
 	let unitIndex = 0;
 	const TWO_TO_TEN = 2 ** 10;
